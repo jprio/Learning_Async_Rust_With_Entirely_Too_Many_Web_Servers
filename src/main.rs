216c1f21416ef921bd57971b0114f0dbcd462e40
jprio <jeanpierre.rio@gmail.com>
2023-08-27 09:49:00 +0200

    A multiplexed server

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use std::collections::HashMap;
 use std::net::TcpListener;
 use chrono::{ DateTime, Utc };
 use std::io;
 use std::io::Read;
 use std::io::Write;
+use std::os::windows::io::AsRawSocket;
+use polling::{ Event, Poller };
 
 use log::{ info, warn, error };
 
@@ -19,35 +22,65 @@ enum ConnectionState {
 }
 fn main() {
     env_logger::init();
+    let epoll = Poller::new().unwrap();
+
     let listener = TcpListener::bind("localhost:3000").unwrap();
-    let mut connections = Vec::new();
+    listener.set_nonblocking(true).unwrap();
+    let listener_fd = listener.as_raw_socket() as usize;
+    //epoll.add(&listener, Event::readable(key)).unwrap();
+    epoll.add(&listener, Event::all(listener_fd)).unwrap();
+
+    let mut connections = HashMap::new();
+    let mut events = Vec::new();
 
     loop {
-        match listener.accept() {
-            Ok((connection, _)) => {
-                info!("New connection accepted");
-                connection.set_nonblocking(true).unwrap();
-                let state = ConnectionState::Read {
-                    request: [0u8; 1024],
-                    read: 0,
-                };
+        // Wait for at least one I/O event.
+        events.clear();
+        epoll.wait(&mut events, Some(std::time::Duration::from_millis(1000))).unwrap();
+        let mut completed = Vec::new();
 
-                connections.push((connection, state));
+        'next: for ev in &events {
+            let fd = ev.key;
+
+            if fd == listener_fd {
+                info!("listener event");
+                // Perform a non-blocking accept operation.
+                match listener.accept() {
+                    Ok((connection, _)) => {
+                        info!("New connection accepted");
+                        connection.set_nonblocking(true).unwrap();
+                        epoll
+                            .add(&connection, Event::all(connection.as_raw_socket() as usize))
+                            .unwrap();
+                        let state = ConnectionState::Read {
+                            request: [0u8; 1024],
+                            read: 0,
+                        };
+                        info!("inset connection {} in connections", connection.as_raw_socket());
+                        connections.insert(connection.as_raw_socket() as usize, (
+                            connection,
+                            state,
+                        ));
+                    }
+                    Err(e) if e.kind() == io::ErrorKind::WouldBlock => {}
+                    Err(e) => panic!("{e}"),
+                }
+
+                // Set interest in the next readability event.
+                epoll.modify(&listener, Event::readable(listener_fd)).unwrap();
+                continue 'next;
             }
-            Err(e) if e.kind() == io::ErrorKind::WouldBlock => {}
-            Err(e) => panic!("{e}"),
-        }
+            // otherwise, a connection is ready
+            info!("get connection {}", fd);
+            let (connection, state) = connections.get_mut(&fd).unwrap();
 
-        let mut completed: Vec<usize> = Vec::new();
-        'next: for (i, (connection, state)) in connections.iter_mut().enumerate() {
-            info!("Handling conn {}", i);
             if let ConnectionState::Read { request, read } = state {
                 loop {
                     // try reading from the stream
                     match connection.read(&mut request[*read..]) {
                         Ok(0) => {
                             println!("client disconnected unexpectedly");
-                            completed.push(i);
+                            completed.push(fd);
                             continue 'next;
                         }
                         Ok(n) => {
@@ -88,7 +121,7 @@ fn main() {
                     match connection.write(&response[*written..]) {
                         Ok(0) => {
                             println!("client disconnected unexpectedly");
-                            completed.push(i);
+                            completed.push(fd);
                             continue 'next;
                         }
                         Ok(n) => {
@@ -113,7 +146,7 @@ fn main() {
             if let ConnectionState::Flush = state {
                 match connection.flush() {
                     Ok(_) => {
-                        completed.push(i);
+                        completed.push(fd);
                     }
                     Err(e) if e.kind() == io::ErrorKind::WouldBlock => {
                         // not ready yet, move on to the next connection
@@ -123,67 +156,7 @@ fn main() {
                 }
             }
         }
-        for i in completed.into_iter().rev() {
-            connections.remove(i);
-        }
     }
-}
-
-fn handle_connection(mut connection: std::net::TcpStream) -> io::Result<()> {
-    let now: DateTime<Utc> = Utc::now();
-    let mut request = [0u8; 1024];
-    let mut read = 0;
-    info!("{:?} : handle connection", now);
-    loop {
-        // try reading from the stream
-        let num_bytes = connection.read(&mut request[read..])?;
-
-        // the client disconnected
-        if num_bytes == 0 {
-            println!("client disconnected unexpectedly");
-            return Ok(());
-        }
-
-        // keep track of how many bytes we've read
-        read += num_bytes;
-        // have we reached the end of the request?
-        if request.get(read - 4..read) == Some(b"\r\n\r\n") {
-            break;
-        }
-    }
-    let request = String::from_utf8_lossy(&request[..read]);
-    info!("{request}");
-    // "Hello World!" in HTTP
-    let response = concat!(
-        "HTTP/1.1 200 OK\r\n",
-        "Content-Length: 12\n",
-        "Connection: close\r\n\r\n",
-        "Hello world!"
-    );
-
-    let mut written = 0;
-
-    loop {
-        // write the remaining response bytes
-        let num_bytes = connection.write(response[written..].as_bytes())?;
-
-        // the client disconnected
-        if num_bytes == 0 {
-            println!("client disconnected unexpectedly");
-            return Ok(());
-        }
-
-        written += num_bytes;
-
-        // have we written the whole response yet?
-        if written == response.len() {
-            break;
-        }
-    }
-    // flush the response
-    connection.flush();
-
-    Ok(())
 }
 
 #[cfg(test)]
